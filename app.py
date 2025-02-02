567bd1a51b87ddc41f26fb3de394e137d7add63d
ibnissaghyr <ibnissaghyrhicham@gmail.com>
2025-02-02 15:17:24 +0100

    commit

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask
 from src.logger import logging
+from src.exception import CustomException
+import sys, os
 
 
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    logging.info("we are testing")
-    return "Welcome  chef"
+    try:
+        raise Exception("We are testing our session")
+    except Exception as e:
+        abc=CustomException(e,sys)
+        logging.info(abc.error_message)
+        return "We are workingg"
 
 if __name__=="__main__":
     app.run(debug=True)
